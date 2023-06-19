fd17fc031d528b22f87c6ea5392285decbabcb1a
unchaptered <workstation19961002@gmail.com>
2023-06-19 00:42:55 +0000

    Create : RdsProvider

--- src/modules/provider/rds_provider.py
+++ src/modules/provider/rds_provider.py
@@ -1,4 +1,6 @@
 from mysql.connector import pooling
+from contextlib import contextmanager
+from pymysql.cursors import DictCursor
 
 # Modules
 from src.modules.provider.config_provider import configProvider
@@ -11,8 +13,8 @@ rdsProviderPoolConfig = {
     'database': rdsProviderconfig.DATABASE_NAME
 }
 rds_pool = pooling.MySQLConnectionPool(
-    pool_name = "sample",
-    pool_size = 10,
+    pool_name   = "sample",
+    pool_size   = 10,
     **rdsProviderPoolConfig
 )
 
@@ -20,3 +22,14 @@ class RdsProvider():
     
     def get_connection(self) -> pooling.PooledMySQLConnection:
         return rds_pool.get_connection()
+    
+    @contextmanager
+    def get_auto_connection(self) -> pooling.PooledMySQLConnection:
+        
+        conn = rds_pool.get_connection()
+        
+        try:
+            yield conn
+        finally:
+            conn.close()
+            
